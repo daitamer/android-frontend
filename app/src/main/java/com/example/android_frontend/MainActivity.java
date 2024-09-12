2d785ea8f6f2a15467f23a7418b91e76fdf3cada
Despina Ait-Amer <despinaaitamer@Despinas-MacBook-Air.local>
2024-09-12 16:44:34 +0100

    feat: main functionality

--- app/src/main/java/com/example/android_frontend/MainActivity.java
+++ app/src/main/java/com/example/android_frontend/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.android_frontend;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,36 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+int calculate(int input1, int input2){
+    return input1 + input2;
+
+}
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        EditText input1 = findViewById(R.id.input1);
+        EditText input2 = findViewById(R.id.input2);
+
+        int inp1 = Integer.parseInt(input1.getText().toString());
+        int inp2 = Integer.parseInt(input2.getText().toString());
+
+        TextView result = findViewById(R.id.result);
+
+        Button button = findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result.setText(calculate(inp1, inp2));
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
