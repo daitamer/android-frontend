23384082dea09ebc9f7e07ba1f1e51e7e1124d3a
AbiPetheram <abi.gill97@gmail.com>
2024-09-13 09:38:33 +0100

    fix: move get input to within onClick method

--- app/src/main/java/com/example/android_frontend/MainActivity.java
+++ app/src/main/java/com/example/android_frontend/MainActivity.java
@@ -13,11 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    int calculate(int input1, int input2){
+        return input1 + input2;
 
-int calculate(int input1, int input2){
-    return input1 + input2;
-
-}
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +28,6 @@ int calculate(int input1, int input2){
         EditText input1 = findViewById(R.id.input1);
         EditText input2 = findViewById(R.id.input2);
 
-        int inp1 = Integer.parseInt(input1.getText().toString());
-        int inp2 = Integer.parseInt(input2.getText().toString());
-
         TextView result = findViewById(R.id.result);
 
         Button button = findViewById(R.id.button1);
@@ -39,7 +35,9 @@ int calculate(int input1, int input2){
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                result.setText(calculate(inp1, inp2));
+                int inp1 = Integer.parseInt(input1.getText().toString());
+                int inp2 = Integer.parseInt(input2.getText().toString());
+                result.setText(String.valueOf(calculate(inp1, inp2)));
             }
         });
 
